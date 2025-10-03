104749f77b28c7017f114348d6e3476ba49ad1d9
Pierral87 <pa.lacaze1@gmail.com>
2025-10-03 13:58:50 +0200

    Exercices Cookie & Session

---
+++ PHP/04-COOKIE/exoCookie/exoLangue.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+
+    EXERCICE COOKIE :
+            Mémorisation d'un choix de langue par l'utilisateur : 
+
+                Etapes : 
+                    - 1 Créer 4 liens HTML représentant des langues 
+                    - 2 Via le GET, transmettre les informations de la langue cliquée
+                    - 3 En fonction de la langue cliquée, créer un cookie correspondant
+                    - 4 Vérifier le fonctionnement en revenant sur la page pour voir si la langue a été mémorisée (afficher la langue sélectionnée ou une phrase dans la langue en question)
+                    - 5 Bien faire en sorte que le choix de langue soit cohérent (quelle serait la priorité entre le cookie, le choix utilisateur, le choix par défaut)
+                    - 6 Faire en sorte que la langue par défaut soit celle du navigateur 
+
+*/ 
