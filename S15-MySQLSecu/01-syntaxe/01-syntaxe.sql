86395a86c5f4967e868aea5838dc483d30274f7b
Pierral87 <pa.lacaze1@gmail.com>
2025-10-28 10:33:43 +0100

    MySQL Chapter 1 Syntax Over

--- S15-MySQLSecu/01-syntaxe/01-syntaxe.sql
+++ S15-MySQLSecu/01-syntaxe/01-syntaxe.sql
@@ -489,7 +489,13 @@ SELECT COUNT(*) AS nombre_employes, service FROM employes GROUP BY service;
 -- Il est possible de mettre une condition sur un GROUP BY    :  HAVING 
 -- Nombre d'employés par service, pour les services ayant plus de 2 employés 
 SELECT COUNT(*), service FROM employes GROUP BY service HAVING COUNT(*) > 2;
-
++----------+--------------+
+| COUNT(*) | service      |
++----------+--------------+
+|        6 | commercial   |
+|        3 | secretariat  |
+|        3 | informatique |
++----------+--------------+
 
 --------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------
@@ -557,23 +563,142 @@ DELETE FROM employes WHERE id_employes > 990;
 --------------------------------------------------------------------------
 
 -- 1 -- Afficher la profession de l'employé 547.
+SELECT service FROM employes WHERE id_employes = 547;
++------------+
+| service    |
++------------+
+| commercial |
++------------+
 -- 2 -- Afficher la date d'embauche d'Amandine.	
+SELECT date_embauche FROM employes WHERE prenom = "Amandine";
++---------------+
+| date_embauche |
++---------------+
+| 2014-01-23    |
++---------------+
 -- 3 -- Afficher le nom de famille de Guillaume	
+SELECT nom FROM employes WHERE prenom = "Guillaume";
++--------+
+| nom    |
++--------+
+| Miller |
++--------+
 -- 4 -- Afficher le nombre de personne ayant un n° id_employes commençant par le chiffre 5.	
+SELECT COUNT(*) AS nbr_id_5 FROM employes WHERE id_employes LIKE "5%";
++----------+
+| nbr_id_5 |
++----------+
+|        3 |
++----------+
 -- 5 -- Afficher le nombre de commerciaux.
+SELECT COUNT(*) AS nbr_commerciaux FROM employes WHERE service = "commercial";
++-----------------+
+| nbr_commerciaux |
++-----------------+
+|               6 |
++-----------------+
 -- 6 -- Afficher le salaire moyen des informaticiens 
--- 7 -- Afficher les 5 premiers employés après avoir classé leur nom de famille par ordre alphabétique. 
+SELECT ROUND(AVG(salaire)) AS moyenne_informaticiens FROM employes WHERE service = "informatique";
++------------------------+
+| moyenne_informaticiens |
++------------------------+
+|                   2617 |
++------------------------+
+-- 7 -- Afficher les 5 premiers employés après avoir classé leur nom de famille par ordre alphabétique.
+ SELECT prenom, nom FROM employes ORDER BY nom LIMIT 5;
+ +---------+----------+
+| prenom  | nom      |
++---------+----------+
+| Laura   | Blanchet |
+| Daniel  | Chevel   |
+| Melanie | Collier  |
+| Julien  | Cottet   |
+| Thierry | Desprez  |
++---------+----------+
 -- 8 -- Afficher le coût des commerciaux sur 1 année.	
+SELECT SUM(salaire * 12) AS cout_commerciaux FROM employes WHERE service = "commercial";
++------------------+
+| cout_commerciaux |
++------------------+
+|           184200 |
++------------------+
 -- 9 -- Afficher le salaire moyen par service. 
+SELECT service, ROUND(AVG(salaire)) AS salaire_moyen FROM employes GROUP BY service;
 -- 10 -- Afficher le nombre de recrutement sur l'année 2010
+SELECT COUNT(*) AS recrutement_2010 FROM employes WHERE date_embauche BETWEEN "2010-01-01" AND "2010-12-31";
+SELECT COUNT(*) AS recrutement_2010 FROM employes WHERE YEAR(date_embauche) = 2010;
+SELECT COUNT(*) AS recrutement_2010 FROM employes WHERE date_embauche LIKE "2010%";
++------------------+
+| recrutement_2010 |
++------------------+
+|                2 |
++------------------+
 -- 11 -- Afficher le salaire moyen appliqué lors des recrutements sur la période allant de 2015 a 2017
+SELECT AVG(salaire) AS salaire_moyen FROM employes WHERE YEAR(date_embauche) BETWEEN 2015 AND 2017;
++---------------+
+| salaire_moyen |
++---------------+
+|          2555 |
++---------------+
 -- 12 -- Afficher le nombre de service différent 
+SELECT COUNT(DISTINCT service) AS nbr_services FROM employes; 
++--------------+
+| nbr_services |
++--------------+
+|            9 |
++--------------+
 -- 13 -- Afficher tous les employés sauf ceux du service production et secrétariat
+SELECT * FROM employes WHERE service NOT IN ("production", "secretariat");
 -- 14 -- Afficher conjointement le nombre d'homme et de femme dans l'entreprise
+SELECT sexe, COUNT(*) AS nbr FROM employes GROUP BY sexe;
+SELECT SUM(sexe = 'f') AS femmes, SUM(sexe = 'm') AS hombres FROM employes;
++------+-----+
+| sexe | nbr |
++------+-----+
+| m    |  11 |
+| f    |   9 |
++------+-----+
 -- 15 -- Afficher les commerciaux ayant été recrutés avant 2012 de sexe masculin et gagnant un salaire supérieur a 2500 €
+SELECT * FROM employes 
+WHERE service = "commercial" 
+AND date_embauche < "2012-01-01" 
+AND sexe = "m" 
+AND salaire > 2500;
++-------------+--------+--------+------+------------+---------------+---------+
+| id_employes | prenom | nom    | sexe | service    | date_embauche | salaire |
++-------------+--------+--------+------+------------+---------------+---------+
+|         415 | Thomas | Winter | m    | commercial | 2011-05-03    |    3550 |
++-------------+--------+--------+------+------------+---------------+---------+
 -- 16 -- Qui a été embauché en dernier 
+SELECT * FROM employes ORDER BY date_embauche DESC LIMIT 1;
+SELECT * FROM employes WHERE date_embauche = (SELECT MAX(date_embauche) FROM employes);
++-------------+-----------+--------+------+-----------+---------------+---------+
+| id_employes | prenom    | nom    | sexe | service   | date_embauche | salaire |
++-------------+-----------+--------+------+-----------+---------------+---------+
+|         990 | Stephanie | Lafaye | f    | assistant | 2017-03-01    |    1775 |
++-------------+-----------+--------+------+-----------+---------------+---------+
 -- 17 -- Afficher les informations sur l'employé du service commercial gagnant le salaire le plus élevé
+SELECT * FROM employes WHERE service = "commercial" ORDER BY salaire DESC LIMIT 1;
++-------------+--------+--------+------+------------+---------------+---------+
+| id_employes | prenom | nom    | sexe | service    | date_embauche | salaire |
++-------------+--------+--------+------+------------+---------------+---------+
+|         415 | Thomas | Winter | m    | commercial | 2011-05-03    |    3550 |
++-------------+--------+--------+------+------------+---------------+---------+
 -- 18 -- Afficher le prénom du comptable gagnant le meilleur salaire 
+SELECT prenom FROM employes WHERE service = "comptabilite" ORDER BY salaire DESC LIMIT 1;
++---------+
+| prenom  |
++---------+
+| Fabrice |
++---------+
 -- 19 -- Afficher le prénom de l'informaticien ayant été recruté en premier 
+SELECT prenom FROM employes WHERE service = "informatique" ORDER BY date_embauche ASC LIMIT 1;
++---------+
+| prenom  |
++---------+
+| Mathieu |
++---------+
 -- 20 -- Augmenter chaque employé de 100 €
+UPDATE employes SET salaire = salaire + 100;
 -- 21 -- Supprimer les employés du service secrétariat
+DELETE FROM employes WHERE service = "secretariat";
