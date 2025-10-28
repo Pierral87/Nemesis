ed2c7b02dfbfc893f0f459deb27f1f4f5f22ea58
Pierral87 <pa.lacaze1@gmail.com>
2025-10-28 11:21:57 +0100

    MySQL Chapter 2 Exercices Imbrik Start

--- S15-MySQLSecu/02-imbrication-jointure/02-imbrication-jointure.sql
+++ S15-MySQLSecu/02-imbrication-jointure/02-imbrication-jointure.sql
@@ -47,3 +47,51 @@ INSERT INTO emprunt (id_emprunt, id_livre, id_abonne, date_sortie, date_rendu) V
 (6, 105, 2, '2017-01-02', '2017-01-15'),
 (7, 105, 3, '2017-02-15', NULL),
 (8, 100, 2, '2017-02-20', NULL);
+
+-- Quels sont les id_livre des livres qui n'ont pas été rendu à la bibliothèque ? 
+SELECT id_livre FROM emprunt WHERE date_rendu IS NULL;
+-- Attention, la valeur NULL se teste avec IS NULL ou IS NOT NULL 
++----------+
+| id_livre |
++----------+
+|      105 |
+|      100 |
++----------+
+
+-- Pour avoir les titres des livres ? Ces informations sont sur une autre table...
+-- 2 possibilités :
+    -- Requêtes imbriquées
+    -- Requêtes en jointure 
+
+---------------------------------------------------------------------
+---------------------------------------------------------------------
+------- REQUETES IMBRIQUEES (sur plusieurs tables) ------------------
+---------------------------------------------------------------------
+---------------------------------------------------------------------
+---------------------------------------------------------------------
+
+-- Quels sont les titres des livres qui n'ont pas été rendu à la bibliothèque ?
+    -- Concept : C'est une requête à l'intérieur d'une autre requête 
+        -- J'ai besoin du résultat de la requête de "sous niveau" pour mener à bien la requête de "premier niveau"
+
+-- Ce que je veux afficher ? Les titres, ils sont présents sur la table "livre"
+    -- Quel est le champ commun entre mes tables ? Forcément la primary key/foreign key, ici c'est id_livre
+        -- Le résultat de la deuxième requête me permet de sortir les titres des livres de la première requête
+    SELECT titre FROM livre WHERE id_livre IN (SELECT id_livre FROM emprunt WHERE date_rendu IS NULL);
++-------------------------+
+| titre                   |
++-------------------------+
+| Une vie                 |
+| Les Trois Mousquetaires |
++-------------------------+
+    -- Pour une requête imbriquée entre plusieurs tables il faut forcément une association/relation afin d'avoir un champ commun (PK/FK)
+
+-- EXERCICE 1: Quels sont les prénoms des abonnés n'ayant pas rendu un livre à la bibliotheque.
+-- EXERCICE 2: Nous aimerions connaitre le(s) n° des livres empruntés par Chloé
+-- EXERCICE 3: Affichez les prénoms des abonnés ayant emprunté un livre le 07/12/2016.
+-- EXERCICE 4: combien de livre Guillaume a emprunté à la bibliotheque ?
+-- EXERCICE 5: Affichez la liste des abonnés ayant déjà emprunté un livre d'Alphonse Daudet
+-- EXERCICE 6: Nous aimerions connaitre les titres des livres que Chloe a emprunté à la bibliotheque.
+-- EXERCICE 7: Nous aimerions connaitre les titres des livres que Chloe n'a pas emprunté à la bibliotheque.
+-- EXERCICE 8: Nous aimerions connaitre les titres des livres que Chloe a emprunté à la bibliotheque ET qui n'ont pas été rendu.
+-- EXERCICE 9 :  Qui a emprunté le plus de livre à la bibliotheque ?
